7a8119b76593b98239ac55ed4d4f2eb133c68902
djviodes <djviodes26@gmail.com>
2020-11-06 08:26:44 -0800

    Made some changes and got Provider up and going on index.js

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./components/App";
+import thunk from "redux-thunk";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import App from "./components/App";
+import { reducer } from './store/reducers/reducer';
+
+import "./index.css";
+
+const store = createStore(reducer, applyMiddleware(thunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
